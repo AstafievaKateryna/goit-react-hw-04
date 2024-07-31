24e4506e663c023e090151d8283e4351b24d02c0
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-07-31 22:11:53 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,110 @@
 import { useEffect, useState } from "react";
-import contacts from "./contact.json";
 import "./App.css";
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from "./components/ContactList/ContactList";
+import { fetchData } from "./services/api";
 
-function App() {
-  const localeContacts = () => {
-    const savedContacts = localStorage.getItem("Contacts");
-    return savedContacts ? JSON.parse(savedContacts) : contacts;
+import Keys from "./services/ApiKEY.json";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import SearchBar from "./components/SearchBar/SearchBar";
+import toast, { Toaster } from "react-hot-toast";
+import { ProgressBar } from "react-loader-spinner";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
+
+const App = () => {
+  const notify = (message) => toast.error(message);
+
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [error, setError] = useState(false);
+  const [modalUrls, setModalUrls] = useState("");
+  const [alt, setAlt] = useState("");
+  const [modalIsOpened, setModalIsOpened] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const search = form.query.value;
+    if (!search) {
+      return notify("Please enter query");
+    }
+    setQuery(search);
+    setImages([]);
+    setLoading(true);
+    setError(false);
+    form.reset();
   };
-  const [contact, setContact] = useState(localeContacts);
-
-  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    window.localStorage.setItem("Contacts", JSON.stringify(contact));
-  }, [contact]);
+    if (query) {
+      const ApiKey = Keys.ApiKey;
 
-  const addContact = (newContact) => {
-    setContact((prevContact) => {
-      return [...prevContact, newContact];
-    });
+      const fetchDatas = async () => {
+        try {
+          setError(false);
+          const res = await fetchData(ApiKey, query, page);
+
+          setImages((prev) => [...prev, ...res.results]);
+          setIsVisible(false);
+          if (page < res.total_pages) {
+            setIsVisible(true);
+          }
+        } catch (error) {
+          setIsVisible(false);
+          setError(true);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchDatas();
+    }
+  }, [query, page]);
+  const loadMore = () => {
+    setPage((prev) => prev + 1);
   };
-  const handleDelete = (contactId) => {
-    setContact((prevContact) => {
-      return prevContact.filter((contact) => contact.id !== contactId);
-    });
+  const openModal = (alt, modalUrls) => {
+    setModalIsOpened(true);
+    setAlt(alt);
+    setModalUrls(modalUrls);
   };
-  const searchContact = contact.filter((cont) =>
-    cont.name.toLowerCase().includes(filter.toLowerCase())
-  );
-  const changeFilter = (value) => {
-    setFilter(value);
+  const closeModal = () => {
+    setModalIsOpened(false);
+    setAlt("");
+    setModalUrls("");
   };
 
   return (
     <div>
-      <h1>Phonebook</h1>
-      <div className="card">
-        <ContactForm addContact={addContact} />
-        <SearchBox changeFilter={changeFilter} filter={filter} />
-      </div>
-      <ContactList handleDelete={handleDelete} contact={searchContact} />
+      Gallery
+      <SearchBar handleSubmit={handleSubmit} />
+      <Toaster />
+      {loading && (
+        <ProgressBar
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      )}
+      {error && notify("Please try again...")}
+      <ImageGallery
+        images={images}
+        openModal={openModal}
+        closeModal={closeModal}
+      />
+      {isVisible && <LoadMoreBtn loadMore={loadMore} />}
+      <ImageModal
+        openModal={modalIsOpened}
+        closeModal={closeModal}
+        modalUrls={modalUrls}
+        alt={alt}
+      />
     </div>
   );
-}
+};
 
 export default App;
